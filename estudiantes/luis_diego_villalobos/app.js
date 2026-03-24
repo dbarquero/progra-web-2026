7179092d55d8a078f8a48f8e359ec606740a6391
Luis Diego <luisdiego2411@gmail.com>
2026-03-24 16:33:59 -0600

    Ajusta app.js y nombre.html

--- estudiantes/luis_diego_villalobos/app.js
+++ estudiantes/luis_diego_villalobos/app.js
@@ -10,6 +10,10 @@ app.get('/saludo', (req, res) => {
   res.sendFile(path.join(__dirname, 'saludo.html'));
 });
 
+app.get('/nombre', (req, res) => {
+  res.sendFile(path.join(__dirname, 'nombre.html'));
+});
+
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
 });
