b1183bed1ac2d51c7b7e1b4214836dacbc23ac5a
Nellyjm2 <nellyjimenezmndz@gmail.com>
2026-04-14 21:19:55 -0600

    Subo carpeta Nelly Jimenez Mendez

---
+++ estudiantes/Nelly-Jimenez-Mendez/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get( '/', ( req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen( 3000, () => {
+    console.log('Server corriendo en http://localhost:3000')
+
+    
+})
